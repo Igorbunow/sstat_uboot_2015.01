f367247f57159f5d44c81faf7aa45f6c6a310bcb
Igor Gorbunov <igor@gorbunov.tel>
2023-09-14 05:26:41 +0300

    Remove autogened files

--- include/ms_version.h
+++
@@ -1,7 +0,0 @@
-// $Change:  $
-#define MVXV_HEAD_VER   {'4'}
-#define MVXV_LIB_TYPE   {'#', '#'}
-#define MVXV_CHIP_ID    {'I', '2', 'M', '#'}
-#define MVXV_CHANGELIST {'g', '#', '#', '#', '#', '#', '#', '#'}
-#define MVXV_COMP_ID     {'C', 'M', '_', 'U', 'B', 'T', '1', '5', '0', '1'}
-
